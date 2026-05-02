04840119413497f345f21f82842c2d8abda772e4
uluana_roshina <uluanasev15@gmail.com>
2026-05-02 11:39:16 +0300

    done with domain models

---
+++ EducationQuestions/src/EducationQuestions.Domain/Reports/Status.cs
@@ -0,0 +1,9 @@
+namespace EducationQuestions.Domain.Reports;
+
+public enum Status
+{
+    Open, 
+    InProgress,
+    Resolved,
+    Dismissed
+}
---
+++ EducationQuestions/src/EducationQuestions.Domain/Tags/Tag.cs
@@ -0,0 +1,8 @@
+namespace EducationQuestions.Domain.Tags;
+
+public class Tag
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    
+}
